cb77b0541df5080cc5ae8158193964a881873af1
yaalaa <jonh.kelvin@gmail.com>
2021-11-26 16:37:23 +0300

    py - test2 - inhance

--- py/test2.py
+++ py/test2.py
@@ -1,37 +1,31 @@
 
 def water( heights ):
-    total    = 0
-    cnt      = len( heights )
-    idx      = 0
+    total = 0
+    cnt   = len( heights )
+    idx   = 0
     while idx < cnt:
         v = heights[ idx ]
 
         while True:
 
-            # skip same elements
+            # look for height
             idx2  = idx + 1
             while idx2 < cnt:
                 v2 = heights[ idx2 ]
-                if v2 != v: break
+                if v2 >= v: break
                 idx2 += 1
-            if idx2 >= cnt: # at end
-                idx = cnt
+
+            if idx2 <= idx + 1:
+                idx = idx2
                 break
 
-            # look for height
-            idx3  = idx2
-            while idx3 < cnt:
-                v3 = heights[ idx3 ]
-                if v3 >= v: break
-                idx3 += 1
-
-            if idx3 < cnt: # found something
-                total += v * ( idx3 - idx2 ) - sum( heights[ idx2 : idx3 ] )
-                idx = idx3
+            if idx2 < cnt: # found something
+                total += v * ( idx2 - idx - 1 ) - sum( heights[ idx + 1 : idx2 ] )
+                idx = idx2
                 break
 
             # nothing high
-            v = max( heights[ idx2 : ] )
+            v = max( heights[ idx + 1 : ] )
 
     return total
 
