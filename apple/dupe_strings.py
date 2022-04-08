392e5b488ce11d926677bd5132bcc101007786a6
yaalaa <jonh.kelvin@gmail.com>
2022-04-08 20:56:49 +0300

    apple - dupe_strings.py - introduce

---
+++ apple/dupe_strings.py
@@ -0,0 +1,37 @@
+import io
+import re
+import os
+import sys
+
+input_fn = sys.argv[1]
+if not input_fn:
+    print('Error: no input file')
+    exit(1)
+
+print( f'Input: {input_fn}')
+
+if not os.path.isfile(input_fn):
+    print(f'Error: does not exist - {input_fn}')
+    exit(1)
+
+with io.open(input_fn, mode = 'r' ) as f:
+    lines = f.readlines()
+
+data = {}
+for idx, line in enumerate(lines):
+    l = line.strip()
+    if not l                : continue # empty
+    if not l.startswith('"'): continue # comment or something wierd
+    m = re.fullmatch(r'"(?P<key>[^"]+)"\s*=\s*"(?P<val>.*)"\s*;', l)
+    if not m                : continue # bad one
+    key = m.group('key')
+    val = m.group('val')
+    if not key              : continue # still bad
+    if key in data: # dupe
+        print(f'Dupe: "{key}" line {idx} dupes line {data[key]["idx"]}')
+        continue
+    data[key] = {
+        'idx' : idx,
+        'val' : val
+    }
+
