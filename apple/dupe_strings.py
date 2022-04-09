7b749a84c562272ad8200091202e8c27d479174d
yaalaa <jonh.kelvin@gmail.com>
2022-04-09 14:32:53 +0300

    apple - dupe_strings.py - match caseless dupes

--- apple/dupe_strings.py
+++ apple/dupe_strings.py
@@ -17,7 +17,8 @@ if not os.path.isfile(input_fn):
 with io.open(input_fn, mode = 'r' ) as f:
     lines = f.readlines()
 
-data = {}
+data          = {}
+data_caseless = {}
 for idx, line in enumerate(lines):
     l = line.strip()
     if not l                : continue # empty
@@ -29,9 +30,18 @@ for idx, line in enumerate(lines):
     if not key              : continue # still bad
     if key in data: # dupe
         key_pad = ' ' * max( 0, 32 - len( key ) )
-        print(f'Dupe: "{key}"{key_pad} line {idx + 1: 5} dupes line {data[key]["idx"] + 1: 5}')
+        print(f'Dupe: "{key}"{key_pad} line {idx + 1: 5} dupes          line {data[key]["idx"] + 1: 5}')
         continue
     data[key] = {
+        'idx' : idx,
+        'val' : val
+    }
+    key_caseless = key.casefold()
+    if key_caseless in data_caseless:
+        key_pad = ' ' * max( 0, 32 - len( key_caseless ) )
+        print(f'Dupe: "{key_caseless}"{key_pad} line {idx + 1: 5} dupes caseless line {data_caseless[key_caseless]["idx"] + 1: 5}')
+        continue
+    data_caseless[key_caseless] = {
         'idx' : idx,
         'val' : val
     }
