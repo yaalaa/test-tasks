4495eddbbd0d77d208ace3a20eba8a755e132758
yaalaa <jonh.kelvin@gmail.com>
2022-04-15 14:25:52 +0300

    stats - firebase_by_os_version - key prefixes - split by os type

--- stats/firebase_by_os_version.py
+++ stats/firebase_by_os_version.py
@@ -16,8 +16,12 @@ if not os.path.isfile(input_fn):
     print(f'Error: not exist - {input_fn}')
     exit(1)
 
-STRIP_PREFIXES = [
+KEY_PREFIXES   = [
     'iOS',
+    'Android',
+]
+STRIP_PREFIXES = [
+    # 'iOS',
 ]
 STRIP_LEVEL    = 1
 VER_SEP        = '.'
@@ -30,16 +34,29 @@ MARKER         = (
     COL_USERS
 )
 
-def strip_os_version( value ):
+def process_os_version(value):
     # general checks
-    if not value: return value
+    if not value: return
     value = value.strip()
-    if not value: return value
+    if not value: return
+    # key prefixes
+    if KEY_PREFIXES:
+        for prefix in KEY_PREFIXES:
+            if value.startswith( prefix ):
+                value = value[ len( prefix ): ].strip()
+                if not value: return
+                key = prefix
+                break
+        else:
+            # no key prefix
+            return
+    else:
+        key = None
     # prefixes
     for prefix in STRIP_PREFIXES:
         if value.startswith( prefix ):
             value = value[ len( prefix ): ].strip()
-            if not value: return value
+            if not value: return
             break
     # levels
     if STRIP_LEVEL > 0:
@@ -47,7 +64,7 @@ def strip_os_version( value ):
         if len( parts ) >= STRIP_LEVEL:
             parts = parts[ : STRIP_LEVEL ]
             value = VER_SEP.join( parts )
-    return value
+    return key, value
 
 
 
@@ -60,7 +77,6 @@ with io.open( input_fn, mode = 'r' ) as f:
     for row in reader:
 
         if not found_marker:
-            #print(repr(row))
             if row and tuple( row[ : len( MARKER ) ] ) == MARKER:
                 found_marker = {
                     name : index
@@ -74,16 +90,17 @@ with io.open( input_fn, mode = 'r' ) as f:
         else:
             if not row                                       : break    # guess the end
             if len( row ) <= max( col_os_version, col_users ): break    # guess stup
-            val_os_v  = strip_os_version( row[ col_os_version ] )
+            val_key, val_os_v  = process_os_version( row[ col_os_version ] ) or ( None, None )
             if not val_os_v                                  : continue # skip it
-            val_users = row[ col_users      ]
+            val_users = row[ col_users ]
             if not val_users                                 : continue # skip it
             try:
                 val_users_n = int( val_users )
             except ValueError:
                 continue # skip it
             if not val_users_n                               : continue # skip it
-            data[ val_os_v ] = ( data.get( val_os_v ) or 0 ) + val_users_n
+            data    [ val_key  ] = data_key = data.get( val_key ) or {}
+            data_key[ val_os_v ] = ( data_key.get( val_os_v ) or 0 ) + val_users_n
 
 if not data: # nothing fount
     if not found_marker:
@@ -92,8 +109,17 @@ if not data: # nothing fount
     print('Error: no data was found')
     exit(1)
 
-writer = csv.writer( sys.stdout )
-writer.writerow( ( COL_OS_VERSION, COL_USERS ) )
-for os_v in sorted( data.keys() ):
-    writer.writerow( ( os_v, f'{data[ os_v ]}' ) )
+pad_key = False
+writer  = csv.writer( sys.stdout )
+for key in sorted( data.keys() ):
+    if pad_key:
+        for _ in range( 2 ):
+            writer.writerow( () )
+    else:
+        pad_key = True
+    data_key = data[ key ]
+    writer.writerow( ( key, ) )
+    writer.writerow( ( COL_OS_VERSION, COL_USERS ) )
+    for os_v in sorted( data_key.keys() ):
+        writer.writerow( ( os_v, f'{data_key[ os_v ]}' ) )
 
