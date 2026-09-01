6480892829879a3369f704e0ffcc0fff3f27c0a2
yaalaa <jonh.kelvin@gmail.com>
2026-09-01 21:03:28 +0300

    pprog - t2026_09_01_lb_01

---
+++ prog/t2026_09_01_lb_01/task_09_разбор_12_fix.py
@@ -0,0 +1,16 @@
+# подобрать к каждой тройке чисел четвёртое число так, чтобы полученные четыре числа могли быть длинами сторон прямоугольника, иначе приписать 0.
+# определить сумму всех полученных четвёртых чисел
+from pandas import read_excel
+
+
+df = read_excel('9-10.ods', engine = 'odf')
+
+filter_squares = df[df.nunique(axis = 1) == 1]
+
+filter_rectangles = df[df.nunique(axis = 1) == 2]
+
+last_numbers_for_squares = filter_squares.apply(lambda row: row[:1],axis = 1)
+
+last_numbers_for_rectangles = filter_rectangles.apply(lambda row: row.value_counts().index[-1],axis = 1)
+
+print(last_numbers_for_squares['A'].sum() + last_numbers_for_rectangles.sum())
